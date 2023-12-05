2e4d401cedcb36628b9ca0f69b45a2a60446d45e
Mateusz Rojek <mateuszr2047@gmail.com>
2023-12-05 20:21:41 +0100

    Commit1

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        try {
+            Person person = new Person("Jan", "Kowalski", 25, "1234567890");
+            System.out.println(person);
+            person.setAge(-10);
+            System.out.println(person);
+        } catch (NameUndefinedException | IncorrectAgeException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Koniec programu");
+        }
+
     }
 }
